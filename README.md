52382e5b066a1aef00fc23236db7efcf5d7987d9
marcosdellavecchia <33458523+marcosdellavecchia@users.noreply.github.com>
2020-06-26 18:24:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# colorChanger
+ A button that changes the background color of the body every time it's clicked.
---
+++ script.js
@@ -0,0 +1,15 @@
+// Se definen variables que targetean el boton y el body
+const colorBtn = document.querySelector('.colorBtn');
+const bodyBcg = document.querySelector('body');
+
+// Se crea el array con los colores que van a rotar
+const colors = ['yellow', 'red', 'green', 'blue', 'orange', 'purple', 'black', 'aqua', 'fuchsia', 'grey', 'lime', 'maroon', 'navy', 'olive', 'silver', 'teal'];
+
+// Event listener para el boton con una callback function
+colorBtn.addEventListener('click', changeColor);
+
+// Funcion que genera un numero aleatorio y lo aplica al background-color del body
+function changeColor() {
+    let random = Math.floor(Math.random()*colors.length);
+    bodyBcg.style.backgroundColor = colors[random];
+}
